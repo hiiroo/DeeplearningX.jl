14df5174741494aba206f47fe60998b8dbb94ef6
hiiroo <alimrtcyln@gmail.com>
2019-05-25 23:26:23 +0300

    Fixed it.

--- src/loss.jl
+++ src/loss.jl
@@ -43,7 +43,8 @@ p_y: 2D matrix of outputs
 y: Array of correct indices
 "
 function nll(p_y, y; average=true)
-    lp = p_y[nll_indices(p_y, y)]
+    # lp = p_y[nll_indices(p_y, y)]
+    lp = [p_y[y[p_s],p_s] for p_s in 1:size(p_y)[2]]
     average ? mean(lp) : sum(lp)
 end
 
