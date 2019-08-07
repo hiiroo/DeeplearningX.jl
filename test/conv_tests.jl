6fe7b3abf4975ac0c40a058ad2fd5a0f5707c2fa
alimrtcyln <alimrtcyln@gmail.com>
2019-08-07 16:09:05 +0300

    Quick bug fix that enable tests to run on cpu even if there is a gpu.

--- test/conv_tests.jl
+++ test/conv_tests.jl
@@ -2,7 +2,7 @@ module ConvolutionTests
         using Pkg; !(haskey(Pkg.installed(), "CuArrays")) || using CuArrays
         using Deeplearning
         using Test
-
+        Deeplearning.gpu(-1)
 
         w = [1 2 3 ; 4 5 6 ; 7 8 9]
         w = w'
